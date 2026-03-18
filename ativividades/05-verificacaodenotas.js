e8e0217447bf07991ffb0750572d718269d8c835
davipedroza-2026 <davi.pedroza@edu.senai.br>
2026-03-18 11:29:08 -0300

    .

---
+++ ativividades/05-verificacaodenotas.js
@@ -0,0 +1,24 @@
+
+/*****************************************************
+Lista de Exercícios
+****************************************************/
+/*
+Crie uma função que receba a nota de um aluno e
+retorne uma mensagem informando sua situação.
+
+    Considere as seguintes regras:
+
+        Nota menor que 5: reprovado
+        Nota entre 5 e 6.9: recuperação
+        Nota igual ou maior que 7: aprovado
+
+    A função deve retornar uma frase indicando a situação do aluno.
+*/ 
+
+function Verificação (Notas) {
+let frase;
+ if (nota < 5){          
+frase ="voçê está reprovado";
+else if ()
+
+}
