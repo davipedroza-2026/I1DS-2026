a97658a46987fcadac0da656081e092681bc551c
davipedroza-2026 <davi.pedroza@edu.senai.br>
2026-03-24 10:08:29 -0300

    atividade do dia

---
+++ aulas/12-lacoRepeticao.js
@@ -0,0 +1,7 @@
+// O professor te colocou de castigo e pediu para você escrever mil vezes a frase 
+// Eu vou prestar atenção ás aulas e anotar tudo! 
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
